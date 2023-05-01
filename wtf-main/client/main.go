3d8cf8f055243f0358e812f6b41d7caabbe56f69
Hanserfaust <hans.eklund@igw.se>
2023-05-01 15:45:40 +0200

    Updated to wtf-dev-server

--- wtf-main/client/main.go
+++ wtf-main/client/main.go
@@ -15,8 +15,8 @@ import (
 )
 
 const (
-	DefaultHost = "wtf-server.bitknife.se"
-	DefaultPort = "7777"
+	WTFDevServerHost = "wtf-dev-server.bitknife.se"
+	WTFDevServerPort = "7777"
 )
 
 func waitForExitSignals(toServer chan *[]byte) {
@@ -43,8 +43,8 @@ func setupExitTimer(lifetime_sec int) {
 
 func main() {
 	headless := flags.Bool("headless", false, "Start a client headless.")
-	host := flags.StringP("host", "h", DefaultHost, "Server IP or Hostname")
-	port := flags.StringP("port", "p", DefaultPort, "Server Port")
+	host := flags.StringP("host", "h", WTFDevServerHost, "Server IP or Hostname")
+	port := flags.StringP("port", "p", WTFDevServerPort, "Server Port")
 	username := flags.StringP("username", "u", shared.RandName("user"), "Player name")
 	password := flags.StringP("password", "w", "welcome", "Password")
 	lifetime_sec := flags.IntP("lifetime_sec", "l", 0, "Terminate client after this many seconds")
